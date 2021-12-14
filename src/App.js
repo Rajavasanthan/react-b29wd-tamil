fea4d3b11dfd7378652d22b1c60b14cef7aaed6c
rajavasanthan <rajavasanthan@gmail.com>
2021-12-14 11:40:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,142 @@
 import logo from './logo.svg';
 import './App.css';
+import PriceCard from './PriceCard';
 
 function App() {
+  let priceList = [
+    {
+      plan : "FREE",
+      price : 20,
+      currency : "$",
+      period : "month",
+      features : [
+        {
+          name : "Single User",
+          enable : true
+        },
+        {
+          name : "5GB Storage",
+          enable : true
+        },
+        {
+          name : "Unlimited Public Projects",
+          enable : true
+        },
+        {
+          name : "Community Access",
+          enable : true
+        },
+        {
+          name : "Unlimited Private Projects",
+          enable : false
+        },
+        {
+          name : "Dedicated Phone Support",
+          enable : false
+        },
+        {
+          name : "Free Subdomain",
+          enable : false
+        },
+        {
+          name : "Monthly Status Reports",
+          enable : false
+        },
+      ]
+    },
+    {
+      plan : "PLUS",
+      price : 25,
+      currency : "$",
+      period : "month",
+      features : [
+        {
+          name : "5 User",
+          enable : true,
+          isStrong : true
+        },
+        {
+          name : "50GB Storage",
+          enable : true
+        },
+        {
+          name : "Unlimited Public Projects",
+          enable : true
+        },
+        {
+          name : "Community Access",
+          enable : true
+        },
+        {
+          name : "Unlimited Private Projects",
+          enable : true
+        },
+        {
+          name : "Dedicated Phone Support",
+          enable : true
+        },
+        {
+          name : "Free Subdomain",
+          enable : true
+        },
+        {
+          name : "Monthly Status Reports",
+          enable : false
+        },
+      ]
+    },
+    {
+      plan : "PRO",
+      price : 30,
+      currency : "$",
+      period : "month",
+      features : [
+        {
+          name : "Unlimited User",
+          enable : true,
+          isStrong : true
+        },
+        {
+          name : "150GB Storage",
+          enable : true
+        },
+        {
+          name : "Unlimited Public Projects",
+          enable : true
+        },
+        {
+          name : "Community Access",
+          enable : true
+        },
+        {
+          name : "Unlimited Private Projects",
+          enable : true
+        },
+        {
+          name : "Dedicated Phone Support",
+          enable : true
+        },
+        {
+          name : "Free Subdomain",
+          enable : true
+        },
+        {
+          name : "Monthly Status Reports",
+          enable : true
+        }
+      ]
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section class="pricing py-5">
+      <div class="container">
+        <div class="row">
+          {
+            priceList.map(obj => <PriceCard data={obj}></PriceCard>)
+          }
+        </div>
+      </div>
+    </section>
   );
 }
 
